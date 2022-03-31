69e791af8ab86f10ddb53304d24035e2cf056ef0
b3npaii <benjaminmpark23@gmail.com>
2022-03-31 20:46:13 +0000

    strategy almost done

--- tic_tac_toe/strategy1.py
+++ tic_tac_toe/strategy1.py
@@ -1,10 +1,71 @@
 from random import randrange
 
 class strat1:
+
     def select_move(self, board):
         move = 0
-        if board == [0  for _ in range(0, 9)]:
-            move =  4
-        else:
+        if sum(board) == 0:
+            move = 4
+        elif board[0] == board[1] != 0 and board[2] == 0:
             move = 2
+        elif board[0] == board[2] != 0 and board[1] == 0:
+            move = 1
+        elif board[1] == board[2] != 0 and board[0] == 0:
+            move = 0
+        elif board[3] == board[4] != 0 and board[5] == 0:
+            move = 5
+        elif board[3] == board[5] != 0 and board[4] == 0:
+            move = 4
+        elif board[4] == board[5] != 0 and board[3] == 0:
+            move = 3
+        elif board[6] == board[7] != 0 and board[8] == 0:
+            move = 8
+        elif board[6] == board[8] != 0 and board[7] == 0:
+            move = 7
+        elif board[7] == board[8] != 0 and board[6] == 0:
+            move = 6
+        elif board[0] == board[3] != 0 and board[6] == 0:
+            move = 6
+        elif board[0] == board[6] != 0 and board[3] == 0:
+            move = 3
+        elif board[3] == board[6] != 0 and board[0] == 0:
+            move = 0
+        elif board[1] == board[4] != 0 and board[7] == 0:
+            move = 7
+        elif board[1] == board[7] != 0 and board[4] == 0:
+            move = 4
+        elif board[4] == board[7] != 0 and board[1] == 0:
+            move = 1
+        elif board[2] == board[5] != 0 and board[8] == 0:
+            move = 8
+        elif board[2] == board[8] != 0 and board[5] == 0:
+            move = 5
+        elif board[5] == board[8] != 0 and board[2] == 0:
+            move = 2
+        elif board[0] == board[4] != 0 and board[8] == 0:
+            move = 8
+        elif board[0] == board[8] != 0 and board[4] == 0:
+            move = 4
+        elif board[4] == board[8] != 0 and board[0] == 0:
+            move = 0
+        elif board[2] == board[4] != 0 and board[6] == 0:
+            move = 6
+        elif board[2] == board[6] != 0 and board[4] == 0:
+            move = 4
+        elif board[4] == board[6] != 0 and board[2] == 0:
+            move = 2
+        elif board[4] == 0:
+            move = 4
+        elif board[2] == 0:
+            move = 2
+        elif board[6] == 0:
+            move = 6
+        elif board[0] == 0:
+            move = 0
+        elif board[8] == 0:
+            move = 8
+        else:
+            move = randrange(0, 9)
+            while board[move] != 0:
+                move = randrange(0, 9)
         return move
--- tic_tac_toe/test_tic_tac_toe.py
+++ tic_tac_toe/test_tic_tac_toe.py
@@ -2,12 +2,12 @@ from game import Game
 from random_player import randomPlayer
 from strategy1 import strat1
 
-player1 = randomPlayer()
+player1 = strat1()
 player2 = randomPlayer()
 outcomes = {'Tie': 0, 1: 0, 2: 0}
-for i in range(1):
+for i in range(100000):
     game = Game(player2, player1)
-    game.run(log=True)
+    game.run(log=False)
     outcomes[game.win] += 1
     if i % 1000 == 0:
         print(i)
