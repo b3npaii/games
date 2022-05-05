0ddc75b7aa97b9e751366c27c3a9884b449bc4cb
b3npaii <benjaminmpark23@gmail.com>
2022-05-05 01:15:10 +0000

    legal moves, almost done with tree

--- tic_tac_toe/game_tree.py
+++ tic_tac_toe/game_tree.py
@@ -48,4 +48,28 @@ class TicTacToeTree:
         self.generate_tree()
     
     def generate_tree(self):
-        
+        first = Node([0 for i in range(0, 9)])
+        queue = Queue([first])
+
+        while len(queue) > 0:
+            dequeued = queue.dequeue()
+
+            if dequeued.winner != None:
+                continue
+                
+            board = dequeued.state
+            next_player = dequeued.turn
+            moves = board.legal_moves(board)
+            for move in moves:
+                new_board = list(board)
+                new_board[move] = next_player
+                new_node = Node(new_board)
+
+
+    def legal_moves(self, board):
+        legal = []
+        for i in range(0, 9):
+            if board[i] == 0:
+                legal.append(i)
+        return legal
+
