12ae461f0d75f1da9a494be6900455ba803d6eaf
b3npaii <benjaminmpark23@gmail.com>
2022-05-09 22:06:23 +0000

    generate_berry

--- snake/game.py
+++ snake/game.py
@@ -1,3 +1,5 @@
+import random as random
+
 def controls(board):
     x = input()
     while x not in ["w", "a", "s", "d"]:
@@ -8,5 +10,15 @@ def controls(board):
 class game:
     def __init__(self, strategy):
         self.strat = strategy
-        self.board = [[a for i in range(0, 10)] for j in range(0, 10)]
-        
+        self.board = [["." for i in range(0, 10)] for j in range(0, 10)]
+        self.score = 3
+        self.snake = [(4, 1), (4, 2), (4, 3)]
+        self.berry = None
+    
+    def generate_berry(self):
+        possible_places = []
+        for i in range(0, 10):
+            for j in range(0, 10):
+                if board[i][j] == ".":
+                    possible_places.append((i, j))
+        self.berry = possible_places[random.randint(0, len(possible_places) - 1)]
